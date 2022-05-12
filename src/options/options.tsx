17530b87a84552e52113f4dea3368ba61f614b40
Marcin <zasadzkimarcin@gmail.com>
2022-05-12 18:29:18 +0100

    add options page

--- src/options/options.tsx
+++ src/options/options.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { createRoot } from "react-dom/client";
 import "fontsource-roboto";
 import {
@@ -16,22 +16,73 @@ import {
 } from "@mui/material";
 
 import "./options.css";
+import {
+  getStoredOptions,
+  LocalStorageOptions,
+  setStoredOptions,
+} from "../utils/storage";
+
+type FormState = "ready" | "saving";
 
 const App: React.FC<{}> = () => {
+  const [options, setOptions] = useState<LocalStorageOptions | null>(null);
+  const [formState, setFormState] = useState<FormState>("ready");
+
+  useEffect(() => {
+    getStoredOptions().then((options) => setOptions(options));
+  }, []);
+
+  const handleHomeCityChange = (homeCity: string) => {
+    setOptions({
+      ...options,
+      homeCity,
+    });
+  };
+
+  const handleSaveButtonClick = () => {
+    setFormState("saving");
+    setStoredOptions(options).then(() => {
+      setTimeout(() => {
+        setFormState("ready");
+      }, 500);
+    });
+  };
+
+  const isFieldDisabled = formState === "saving";
+
+  if (!options) {
+    return null;
+  }
+
   return (
     <Box mx="10%" my="2%">
       <Card>
         <CardContent>
-          <Grid container direction={"column"}>
+          <Grid container direction={"column"} spacing={4}>
             <Grid item>
               <Typography variant="h4">Weather Extension Options</Typography>
             </Grid>
             <Grid item>
-              <Typography variant="body1">Enter a home city</Typography>
+              <Typography variant="body1">Home city name</Typography>
               <TextField
                 placeholder="Enter a home city"
                 label="Enter a home city"
+                fullWidth
+                value={options.homeCity}
+                onChange={(event) => {
+                  handleHomeCityChange(event.target.value);
+                }}
               />
+            </Grid>
+            <Grid item>
+              <Button
+                variant="contained"
+                color="primary"
+                onClick={handleSaveButtonClick}
+                disabled={isFieldDisabled}
+              >
+                {formState === "ready" ? "Save" : "Saving..."}
+              </Button>
             </Grid>
           </Grid>
         </CardContent>
